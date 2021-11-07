bf64855984b47cd3d67aaf6a7a9a91550d03d1bb
status <marco.costantini7@studio.unibo.it>
2021-11-07 21:18:44 +0100

    implement addNode addEdge nodeSet LinkedNodes methods

--- src/it/unibo/oop/lab06/generics1/GraphImpl.java
+++ src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -1,33 +1,38 @@
 package it.unibo.oop.lab06.generics1;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class GraphImpl<N> implements Graph<N>{
 	
-	Set<N> nodes = new HashSet<>();
-	//la chiave rappresenta la src il valore rappresenta tutti i target del src
 	Map<N, Set<N>> edges = new HashMap<>();
 
 	@Override
 	public void addNode(N node) {
-		nodes.add(node);
+		if(!nodeExist(node)) {
+			edges.put(node, new HashSet<>());
+		}else {
+			return;
+		}
 	}
 
 	@Override
 	public void addEdge(N source, N target) {
-		Set<N> nodesFromSource = edges.get(source);
-		nodesFromSource.add(target);
-		edges.put(source, nodesFromSource);
-		
+		if(nodeExist(source) && nodeExist(target)){
+			edges.get(source).add(target);
+		}
 	}
 
 	@Override
 	public Set<N> nodeSet() {
-		return this.nodes;
+		Set<N> allNodes = new HashSet<>(edges.keySet());
+		return allNodes;
 	}
 
 	@Override
@@ -38,8 +43,22 @@ public class GraphImpl<N> implements Graph<N>{
 
 	@Override
 	public List<N> getPath(N source, N target) {
-		// TODO Auto-generated method stub
-		return null;
+		List<N> pathList = new ArrayList<>();
+		
+		if(nodeExist(source) && nodeExist(target)) {
+			Queue<N> q = new ArrayDeque<>();
+			
+		}
+		
+		return pathList;
+	}
+	
+	private int numberOfNodes() {
+		return edges.keySet().size();
+	}
+	
+	private boolean nodeExist(N node) {
+		return edges.containsKey(node);
 	}
 
 }
